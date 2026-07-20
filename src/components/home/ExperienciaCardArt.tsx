4c64820e6598accd01f0fa7c2fac788d9239fbe7
DIANA MENDOZA <dianamdz@MacBook-Air-de-DIANA.local>
2026-07-20 16:27:05 -0600

    Quitar el destello/textura del lado izquierdo de la tarjeta Experiencia

--- src/components/home/ExperienciaCardArt.tsx
+++ src/components/home/ExperienciaCardArt.tsx
@@ -6,9 +6,6 @@ export default function ExperienciaCardArt() {
       <g filter="url(#filter0_g_2029_5288)">
       <path fillRule="evenodd" clipRule="evenodd" d="M731.843 79.3773L794.999 79L798.01 583.027C798.01 583.027 751.676 445.171 733.465 350.75C715.253 256.328 731.843 79.3773 731.843 79.3773Z" fill="url(#paint0_linear_2029_5288)" fillOpacity="0.4"/>
       </g>
-      <g filter="url(#filter1_g_2029_5288)">
-      <path fillRule="evenodd" clipRule="evenodd" d="M143.159 586.051L80.0019 586.176L79.0001 82.1412C79.0001 82.1412 124.785 220.181 142.619 314.674C160.454 409.167 143.159 586.051 143.159 586.051Z" fill="url(#paint1_linear_2029_5288)" fillOpacity="0.4"/>
-      </g>
       <defs>
       <filter id="filter0_g_2029_5288" x="645.928" y="0" width="231.083" height="662.027" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
       <feFlood floodOpacity="0" result="BackgroundImageFix"/>
@@ -19,20 +16,7 @@ export default function ExperienciaCardArt() {
       <feMergeNode in="displacedImage"/>
       </feMerge>
       </filter>
-      <filter id="filter1_g_2029_5288" x="0" y="3.1412" width="229.688" height="662.035" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-      <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-      <feTurbulence type="fractalNoise" baseFrequency="0.99900001287460327 0.99900001287460327" numOctaves="3" seed="3922" />
-      <feDisplacementMap in="shape" scale="158" xChannelSelector="R" yChannelSelector="G" result="displacedImage" width="100%" height="100%" />
-      <feMerge result="effect1_texture_2029_5288">
-      <feMergeNode in="displacedImage"/>
-      </feMerge>
-      </filter>
       <linearGradient id="paint0_linear_2029_5288" x1="761.321" y1="583.246" x2="764.791" y2="79.2333" gradientUnits="userSpaceOnUse">
-      <stop offset="0.135257" stopColor="#7FDED9"/>
-      <stop offset="0.875" stopColor="#CCFFFB"/>
-      </linearGradient>
-      <linearGradient id="paint1_linear_2029_5288" x1="115.69" y1="82.0683" x2="110.211" y2="586.063" gradientUnits="userSpaceOnUse">
       <stop offset="0.135257" stopColor="#7FDED9"/>
       <stop offset="0.875" stopColor="#CCFFFB"/>
       </linearGradient>
