ee2be21a51a37275f58471b0c2aca99eef5629d7
DIANA MENDOZA <dianamdz@MacBook-Air-de-DIANA.local>
2026-07-20 14:16:57 -0600

    Hacer que el efecto de textura de la tarjeta Experiencia llegue al mismo margen del rectángulo

--- src/components/home/ExperienciaCardArt.tsx
+++ src/components/home/ExperienciaCardArt.tsx
@@ -1,7 +1,7 @@
 // Fondo de la tarjeta Experiencia (rect + blobs de textura) recortado al area exacta de la tarjeta, desde public/Home/EXPERIENCIA/EXPERIENCIA.svg. El icono, titulo y parrafo se dividieron en componentes propios (IconArt/HeadingArt/ParagraphArt) para poder posicionarlos de forma independiente.
 export default function ExperienciaCardArt() {
   return (
-    <svg width="100%" height="100%" viewBox="80 79 722 507" preserveAspectRatio="xMidYMid slice" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+    <svg width="100%" height="100%" viewBox="80 79 722 507" preserveAspectRatio="none" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
       <rect x="80" y="79.0001" width="722" height="507" fill="#85D9D5"/>
       <g filter="url(#filter0_g_2029_5288)">
       <path fillRule="evenodd" clipRule="evenodd" d="M731.843 79.3773L794.999 79L798.01 583.027C798.01 583.027 751.676 445.171 733.465 350.75C715.253 256.328 731.843 79.3773 731.843 79.3773Z" fill="url(#paint0_linear_2029_5288)" fillOpacity="0.4"/>
